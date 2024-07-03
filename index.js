317c0fa69641e4e9962b93f4c23be97c2e4ee21e
Joshua504 <chukwuebukajosh8@gmail.com>
2024-07-03 20:12:01 +0000

    3 Jul 2024, 21:12

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
-// const inputArea = document.querySelector(".textarea");
-// const todoContainer = document.querySelector(".todolist");
-// const addButton = document.querySelector(".add");
-// const statusText = document.querySelector(".status");
+const inputArea = document.querySelector(".textarea");
+const todoContainer = document.querySelector(".todolist");
+const addButton = document.querySelector(".add");
+const statusText = document.querySelector(".status");
 
-// let list = [];
+let list = [];
 
-// addButton.addEventListener("click", () => {
-// 	let userName = inputArea.value;
+addButton.addEventListener("click", () => {
+	let userName = inputArea.value;
 
-// 	if (userName === "") {
-// 		displayStatus("empty input");
-// 		statusText.style.color = "red";
-// 		return;
-// 	}
+	if (userName === "") {
+		displayStatus("empty input");
+		statusText.style.color = "red";
+		return;
+	}
 
-// 	const checkItem = JSON.parse(localStorage.getItem("list"));
+	const checkItem = JSON.parse(localStorage.getItem("list"));
 
-// 	if (checkItem && checkItem.length > 0) {
-// 		checkItem.push(userName);
-// 		list = checkItem;
-// 		localStorage.setItem("list", JSON.stringify(list));
-// 	} else {
-// 		list.push(userName);
-// 		localStorage.setItem("list", JSON.stringify(list));
-// 	}
+	if (checkItem && checkItem.length > 0) {
+		checkItem.push(userName);
+		list = checkItem;
+		localStorage.setItem("list", JSON.stringify(list));
+	} else {
+		list.push(userName);
+		localStorage.setItem("list", JSON.stringify(list));
+	}
 
-// 	addText(userName);
-// 	inputArea.value = "";
-// 	displayStatus("successfully added");
-// 	statusText.style.color = "#37e637";
-// });
+	addText(userName);
+	inputArea.value = "";
+	displayStatus("successfully added");
+	statusText.style.color = "#37e637";
+});
 
-// function addText(txt) {
-// 	let text = `
-//     <div class="textholder">
-// 			<div class="check"></div>
-// 			<p class="display">${txt}</p>
-// 		</div>`;
-// 	todoContainer.innerHTML += text;
-// }
+function addText(txt) {
+	let text = `
+    <div class="textholder">
+			<div class="check"></div>
+			<p class="display">${txt}</p>
+		</div>`;
+	todoContainer.innerHTML += text;
+}
 
-// function displayStatus(stat) {
-// 	statusText.textContent = stat;
-// 	setTimeout(() => {
-// 		statusText.textContent = "";
-// 	}, 3000);
-// }
+function displayStatus(stat) {
+	statusText.textContent = stat;
+	setTimeout(() => {
+		statusText.textContent = "";
+	}, 3000);
+}
 
-// window.addEventListener("DOMContentLoaded", () => {
-// 	let storedList = JSON.parse(localStorage.getItem("list")) || [];
+window.addEventListener("DOMContentLoaded", () => {
+	let storedList = JSON.parse(localStorage.getItem("list")) || [];
 
-// 	if (storedList.length === 0) {
-// 		return;
-// 	} else {
-// 		for (let i = 0; i < storedList.length; i++) {
-// 			addText(storedList[i]);
-// 		}
-// 	}
-// });
+	if (storedList.length === 0) {
+		return;
+	} else {
+		for (let i = 0; i < storedList.length; i++) {
+			addText(storedList[i]);
+		}
+	}
+});
 
-// localStorage.clear();
+// localStorage.clear();p
 
 
 // const checkBox = document.querySelector(".box")
